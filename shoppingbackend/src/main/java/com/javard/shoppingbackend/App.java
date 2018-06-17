54380399456b01228489e2e50848bff78b06ab2f
Prasoon <singhprasoon24@gmail.com>
2018-06-18 01:39:39 +0530

    Successfully setup project

---
+++ shoppingbackend/src/main/java/com/javard/shoppingbackend/App.java
@@ -0,0 +1,13 @@
+package com.javard.shoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
